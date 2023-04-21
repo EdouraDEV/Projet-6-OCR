4252a2ef47e69e1a3d16b096f351732161c506db
EdouraDEV <eddy.bouraba@outlook.fr>
2023-04-21 16:21:39 +0200

    Import de la gallery avec l'API

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,11 +1,20 @@
-const reponse = await fetch("http://localhost:5678/api/works");
-let works;
-works = await reponse.json();
-
-const boutonFiltrer = document.querySelector(".btn-filtrer");
-console.log(boutonFiltrer);
-boutonFiltrer.addEventListener("click", function () {
-  const worksFiltrees = works.filter(function (work) {
-    return works.prix <= 35;
+fetch("http://localhost:5678/api/works")
+  .then((response) => response.json())
+  .then((data) => {
+    debugger;
+    // Traitez les données et créez les éléments <figue> / <img> / <figcaption>
+    data.forEach((works) => {
+      const figure = document.createElement("figure");
+      figure.classList.add("active");
+      const img = document.createElement("img");
+      img.src = works.imageUrl;
+      const figcaption = document.createElement("figcaption");
+      figcaption.textContent = works.title;
+      // Ajout des enfants <img> & <ficaption> au parent <figure>
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+      // Ajoutez les éléments figue à gallery
+      const gallery = document.getElementsByClassName("gallery")[0];
+      gallery.appendChild(figure);
+    });
   });
-});
